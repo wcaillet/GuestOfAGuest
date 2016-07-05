912a56a5fde1e2538ef37ca9998b24bad5043a90
Wensie <wcaillet@MacBook-Pro-3.local>
2016-07-05 11:56:33 -0500

    initial commital

--- src/scripts/Components.js
+++ src/scripts/Components.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import Backbone from 'backbone'
 
 
 const GuestsView = React.createClass({ //
     getInitialState: function(){ //lifecycle method that establishes the starting state of the guestsColl props
         return{
-            guestsColl: this.props.guestsColl
+            guestsColl: this.props.guestsColl,
+            rsvpType: 'all'
         }
     },
+
     componentWillMount: function(){
         // a pub sub
         console.log('bout to mount')
         // call in the collection
-        this.props.guestsColl.on('update', () => { //sets up the event to listen for (update event)
+        var updater = () => { //sets up the event to listen for (update event)
             this.setState({
                 guestsColl: this.state.guestsColl
                 // tells the view to re-render when there is an update to the state of the guestsColl (will only render once)
             })
+        }
+        this.props.guestsColl.on('update', updater)
+        Backbone.Events.on('changeGuestRsvp',updater)
+        Backbone.Events.on('updateViewType',(viewType)=>{
+            this.setState({
+                rsvpType: viewType
+            })
         })
     },
+
     _addGuest: function(guestName) { // a custom function that takes the guestName as input and adds it to the name property on the guestsColl props
         this.props.guestsColl.add({
             name: guestName
@@ -25,16 +36,44 @@ const GuestsView = React.createClass({ //
     },
 
     render: function() { // renders all the componenets that you will see on the page (top level view)
-        console.log(this.props)
+        var collectionToPassDown = this.state.guestsColl  
+        if (this.state.rsvpType !== 'all') {
+            collectionToPassDown = this.state.guestsColl.where({rsvp:this.state.rsvpType})
+        }
         return (
             <div id="guestsViewContainer">
                 <Header /> {/*Header component*/}
                 <GuestAdder _addGuestFromGuestsView={this._addGuest} /> {/*GuestAdder componentthat passes the _addGuest function down to the props of GuestAdder */}
-                <GuestList guestsColl={this.state.guestsColl} /> {/*GuestList component that passes the collection down to the props of GuestList*/}
+                <Tabs />
+                <GuestList guestsColl={collectionToPassDown} /> {/*GuestList component that passes the collection down to the props of GuestList*/}
             </div>
             )
     }
 })
+
+const Tabs = React.createClass({
+
+    _updateRsvpView: function(e) {
+        if (e.target.tagName === 'A') {
+            var viewType = e.target.textContent
+            // clicking on a tab should cause the GuestsView element to update its state
+            Backbone.Events.trigger('updateViewType',viewType)
+        }
+    },
+
+    render: function() {
+        return (
+            <div onClick={this._updateRsvpView} className="tabsContainer">
+                <a href="#guests/yes">yes</a>
+                <a href="#guests/no">no</a>
+                <a href="#guests/maybe">maybe</a>
+                <a href="#guests/pending">pending</a>
+                <a href="#guests/all">all</a>
+            </div>
+            )
+    }
+})
+
 
 const GuestAdder = React.createClass({
 
@@ -73,6 +112,7 @@ const Guest = React.createClass({
         this.props.guestModel.set({ //sets the rsvp value to the props of guestModel
             rsvp: e.target.value
         })
+        Backbone.Events.trigger('changeGuestRsvp')
     },
 
     _killGuest: function() {
@@ -84,24 +124,16 @@ const Guest = React.createClass({
         var rsvpVal = this.props.guestModel.get('rsvp')
         //the value of rsvp is set by getting the data grom the rsvp property of the guestModel
 
-        // ternary operator alert!
-        var selectedVals = {
-            pending: rsvpVal === 'pending' ? 'selected': '',
-            yes: rsvpVal === 'yes' ? 'selected': '',
-            no: rsvpVal === 'no' ? 'selected': '',
-            maybe: rsvpVal === 'maybe' ? 'selected': '',
-
-            // alternative to if/eslse function. if the consition (first value) is true it will return the first expression (second value) if it is false it will return the second expression (third value)
-        }
 
         return (
             <div className="guest">
                 <span className="name">{this.props.guestModel.get('name')}</span>
+                <span className="rsvp">{rsvpVal}</span>
                 <select onChange={this._changeRSVP}> {/*renders whichever option is selected and triggers the _changeRSVP method to set the rsvp on the props of Guest componenet*/}
-                    <option value="pending" selected={selectedVals.pending} >pending</option>
-                    <option value="yes" selected={selectedVals.yes} >yes</option>
-                    <option value="no" selected={selectedVals.no} >no</option>
-                    <option value="maybe" selected={selectedVals.maybe} >maybe</option>
+                    <option value="pending" >pending</option>
+                    <option value="yes" >yes</option>
+                    <option value="no" >no</option>
+                    <option value="maybe" >maybe</option>
                 </select>
                 <button onClick={this._killGuest}>X</button>
             </div>
@@ -121,4 +153,7 @@ const Header = React.createClass({ //renders the top of the top of the page
 })
 
 export default GuestsView //makes it available for import so all the modules can communicate with each other
+
+
+
 
